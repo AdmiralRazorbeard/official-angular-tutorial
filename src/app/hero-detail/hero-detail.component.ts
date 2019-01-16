638d794822aaaf2b31069ee4479dde7f566f66f1
Thomas Fournet <t.fournet062@gmail.com>
2019-01-16 16:53:04 +0100

    routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
+import { HeroService } from "../hero.service";
+
+import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
 
 @Component({
@@ -7,9 +12,25 @@ import { Hero } from "../hero";
   styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    //The location is an Angular service for interacting with the browser. You'll use it later to navigate back to the view that navigated here.
+    private location: Location
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
