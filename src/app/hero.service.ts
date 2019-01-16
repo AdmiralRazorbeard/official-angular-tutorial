855173f0b79039619efce4b8db31757b22ce05a3
Thomas Fournet <t.fournet062@gmail.com>
2019-01-16 15:23:19 +0100

    correction

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from "@angular/core";
+
 import { Observable, of } from "rxjs";
+
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 import { MessageService } from "./message.service";
 
 @Injectable({
-  providedIn: "root" //<-- creates a single, shared instance of HeroService and injects into any class that asks for it
+  providedIn: "root"
 })
 export class HeroService {
   constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
     this.messageService.add("HeroService: fetched heroes");
     return of(HEROES);
   }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
 import { HeroService } from "../hero.service";
 
 @Component({
@@ -9,10 +9,15 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
   selectedHero: Hero;
 
+  heroes: Hero[];
+
   constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
@@ -20,9 +25,5 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
-  }
-
-  ngOnInit() {
-    this.getHeroes();
   }
 }
